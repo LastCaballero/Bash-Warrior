3894ed68613a7ce057c03dd4c0c2f80085cbf0da
Goran Mladenovic <59745165+LastCaballero@users.noreply.github.com>
2023-05-21 02:57:55 +0200

    Add files via upload

--- ClassCDivider/classCdivider.sh
+++ ClassCDivider/classCdivider.sh
@@ -3,57 +3,39 @@
 declare -A Networks
 
 NWA=$1
-oct=$(echo $NWA | sed -r 's/[0-9]{1,3}$//')
-MASK=$2
+octets=( $(echo $NWA | tr "." " ") )
+if [[ ${#octets[@]} != 4 ]]; then
+	echo "an ip-address has 4 octets"
+	echo "for example: 200.200.200.1"
+fi
+three=$(sed 's/ /./g' <<<${octets[*]:0:3})
 
-echo $oct
 
-binary=( 
-	$(( 2#10000000 ))
-	$(( 2#11000000 ))
-	$(( 2#11100000 ))
-	$(( 2#11110000 ))
-	$(( 2#11111000 ))
-	$(( 2#11111100 ))
-	$(( 2#11111110 )) 
+dividers=( 
+	[2]=$(( 2#10000000 ))
+	[4]=$(( 2#11000000 ))
+	[8]=$(( 2#11100000 ))
+	[16]=$(( 2#11110000 ))
+	[32]=$(( 2#11111000 ))
+	[64]=$(( 2#11111100 ))
+	[128]=$(( 2#11111110 )) 
 )
 
-declare_pieces(){
-	declare -g pieces=$1
-}
+echo "how many pieces?"
+echo ${!dividers[@]}
+read divider
+divider=${dividers[$divider]} 
 
-select_pieces(){
-	select piece in "2 pieces" "4 pieces" "6 pieces" "8 pieces" "16 pieces" "32 pieces" "64 pieces" "128 pieces"
-		do
-			case $(grep -Eo "[0-9]+" <<<$piece) in
-				2) 	declare_pieces ${binary[0]}; break ;;
-				4) 	declare_pieces ${binary[1]}; break ;;
-				8) 	declare_pieces ${binary[2]}; break ;;
-				16) 	declare_pieces ${binary[3]}; break ;;
-				32) 	declare_pieces ${binary[4]}; break ;;
-				64) 	declare_pieces ${binary[5]}; break ;;
-				128) 	declare_pieces ${binary[6]}; break ;;
-			esac ;
-		break
-	done
-}
-
-
-calculate(){
-	for num in {1..255}
-	do
-		key=$((pieces & num))
-		Networks[$key]+="${oct}${num} "
-	done
-}
-
-select_pieces
-calculate
+for ip in {1..255}
+do
+	key="${three}."$(( $ip & $divider))
+	Networks[$key]+="${three}.$ip "	
+done
 
 for key in ${!Networks[@]}
 do
-	echo "Nethwork: ${oct}${key}:"
-	echo "****************************"
-	echo "${Networks[$key]}" | sed 's/ /&\n/g; s/\n/&&/3; s/\n/ /g'
+	echo
+	echo $key
+	echo "*********************************************"
+	echo ${Networks[$key]} 
 done
-
