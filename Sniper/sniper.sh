205f91a71e4acbae1ced73c9853c54044c6d7a3b
Goran Mladenovic <59745165+LastCaballero@users.noreply.github.com>
2023-05-25 07:27:05 +0200

    Update and rename help1.sh to sniper.sh

---
+++ Sniper/sniper.sh
@@ -0,0 +1,31 @@
+#! /bin/bash
+
+LOCALNETWORK=$( ip route show | grep -Eo "([0-9]{1,3}\.){3}[0-9]{1,3}/[0-9]{1,2}" )
+
+services=$(cat /etc/services | gawk '
+BEGIN {
+     FS = "/| |\t+"
+}
+/^[a-zA-Z]/{
+     print $1, $2, $3
+}
+' | sort -k 3)
+
+tcp_ports=( $(grep -i "tcp" <<<$services) )
+udp_ports=( $(grep -i "udp" <<<$services) )
+
+
+filter_host_ip='grep -Eo "[^ ]+ (([0-9]{1,3}\.){3}[0-9]{1,3}" | sed "s/(//'
+
+
+declare -A nmap_scan_techniques=(
+    ["TCP SYN scan"]="-sS"
+    ["TCP connect scan"]="-sT"
+    ["UDP scan"]="-sU"
+    ["TCP Null scan"]="-sN"
+    ["TCP FIN scan"]="-sF"
+    ["TCP Xmas scan"]="-sX"
+    ["TCP ACK scan"]="-sA"
+    ["TCP Window scan"]="-sW"
+        ["UP Scan"]="-sn"
+)
