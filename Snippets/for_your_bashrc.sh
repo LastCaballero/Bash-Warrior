8eadde7cde6fd5ffc68fdc9fc2a8b44a591b8ba0
Goran Mladenovic <59745165+LastCaballero@users.noreply.github.com>
2023-05-28 13:28:24 +0200

    Create for_your_bashrc.sh

---
+++ Snippets/for_your_bashrc.sh
@@ -0,0 +1,7 @@
+# if nmap doesn´t work in your wsl, install it at windows side
+# the windows exectuable runs also ata wsl side
+alias nmap="nmap.exe"
+
+# some useful environment variables
+export HOMENETWORK=$(ip route show | grep "/24" | cut -f 2 -d " ")
+export GATEWAY=$(ip route show | grep default | cut -f 4 -d " ")
