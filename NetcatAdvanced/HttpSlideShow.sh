f539336ae592cdb1bfc1f6e5920540589d5da57b
Goran Mladenovic <59745165+LastCaballero@users.noreply.github.com>
2024-01-25 09:47:24 +0100

    Update HttpSlideShow.sh

--- NetcatAdvanced/HttpSlideShow.sh
+++ NetcatAdvanced/HttpSlideShow.sh
@@ -1,15 +1,31 @@
-#! /bin/bash
-DIRECTORY=$1
-TIME=$2
+#! /usr/bin/bash
 
-sedcode=''
+if [ ! -z $1 ]; then
+    IMAGEDIR=$1
+    echo $IMAGEDIR
+else
+    echo "directory where images are needed"
+    echo "f.e. ./slideshow Pictures"
+fi
 
+IMAGES=( $( ls $IMAGEDIR/*{jpg,png,JPG,PNG} 2>/dev/null ) )
 
-for image in $( ls *jpg ) 
-do
-	sed $sedcode <<HTTP | netcat -N -l 4000
-	
-	
-	HTTP
+for image in ${IMAGES[@]}; do
+    len=$( wc --bytes $image )
+    type=""
+    case $image in
+        jpg|JPG) type="image/jpeg" ;;
+        png|PNG) type="image/png" ;;
+    esac
+
+{ cat <<HTTP ; cat $image ; }  | netcat -N -l 4000 
+HTTP/1.1 200 Ok
+Content-Type: $type
+Content-Length: $len
+Refresh: 5
+Connection: close
+
+HTTP
 
 done
+
