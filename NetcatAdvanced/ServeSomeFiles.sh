b863146b5a1e42f76d0080ae6da02afc34be82d4
Goran Mladenovic <59745165+LastCaballero@users.noreply.github.com>
2024-01-29 09:54:11 +0100

    Update ServeSomeFiles.sh

--- NetcatAdvanced/ServeSomeFiles.sh
+++ NetcatAdvanced/ServeSomeFiles.sh
@@ -1,15 +1,23 @@
 #! /bin/bash
 
-if [ ! -z $1 ]; then
+
+if [ ! -z $1 && cd $1 ]; then
+
+    FILES=( $( ls $1 ) )
         
-        FILES=( $( ls $1 ) )
-        
-        for file in ${FILES[@]}; do
-            cat $file | netcat -N -l 4000
-        done
-    
-    else
-        MESSAGE="No Files available"
+    for file in ${FILES[@]}; do
+        cat $file | netcat -N -l 4000
+    done
+
+elif [ -z $1 ]; then
+
+    FILES=( $( ls $1 ) )    
+    for file in ${FILES[@]}; do
+        cat $file | netcat -N -l 4000
+    done
+
+else
+        MESSAGE="No Files available or some other occured"
         echo $MESSAGE | netcat -N -l 4000
         exit 1
 fi
