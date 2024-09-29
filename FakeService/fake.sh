59f3cc1431633448f9fc02858dd14f7cc943560e
Goran Mladenovic <59745165+LastCaballero@users.noreply.github.com>
2024-09-29 20:43:51 +0200

    Update fake.sh

--- FakeService/fake.sh
+++ FakeService/fake.sh
@@ -1,23 +1,39 @@
 #! /usr/bin/bash
 
 LOGFILE="attacks.log"
+declare -a ports
+
+show_help () {
+    local format="%-20s %s\n"
+    printf  "$format"    "start:"        "fake 4000 {5000..5005}"
+    printf  "$format"    "list:"         "ps -e | sort -k 4"
+    printf  "$format"    "terminate:"    "killall {fake.sh,netcat}"
+}
+
+if ( grep -q -E "help|h" <<<$@ ) || (( $# == 0 ))
+then
+    show_help
+    exit 1
+fi
+
+random_data () {
+    for (( i = 0 ; i < $(( $RANDOM % 50 + 10 )) ; i++ )) ; do
+        echo $(( $RANDOM % 256 ))
+    done
+}
 
 fake_listener () {
     local port=$1
     while true ; do
+        random_data |
         netcat -l $port | read -t 1
-        echo "$(date) attack on port $port" >> $LOGFILE
+        echo "$(date) connect on port $port" >> $LOGFILE
     done
 }
-
- pids=()
-ports=()
 
 for port in $@ ; do
     fake_listener $port &
     ports+=( "$port " )
-     pids+=( "$! " )
 done
 
-echo "Running job-ids: ${pids[@]}"
 echo "Listening ports: ${ports[@]}"
