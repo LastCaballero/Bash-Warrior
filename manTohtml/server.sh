81a8643600774488c46cc27d5d15b6bd8ed5bc7d
Goran Mladenovic <59745165+LastCaballero@users.noreply.github.com>
2024-10-10 07:01:11 +0200

    Create server.sh

---
+++ manTohtml/server.sh
@@ -0,0 +1,34 @@
+#! /usr/bin/bash
+
+file="file.html"
+help=${1:-grep}
+cols=${2:-75}
+env COLUMNS=$cols man $help 2>/dev/null > $file
+
+sed -r -i '1 i \
+<!DOCTYPE html> \
+<html lang="en"> \
+<head> \
+    <meta charset="UTF-8"> \
+    <title>Document</title> \
+</head> \
+<body style="margin: 45px"> \
+<p><pre> ' $file
+sed -r -i '$ a \
+</pre></p> \
+</body> \
+</html>' $file
+
+len=$(wc --bytes $file | cut -f 1 -d " ")
+
+cat <<http  |  
+HTTP/1.1 200 OK
+Connection: close
+Content-length: $len
+
+$( cat $file )
+http
+
+netcat -l 4000
+
+$0 $1 $2
